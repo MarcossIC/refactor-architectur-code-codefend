fe1b2efdfacbee333983b4f84da20f651187b4b6
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-01-03 17:09:04 -0300

    fix bg signin and signup

--- codefend-user-public/src/app/views/pages/auth/AuthPage.jsx
+++ codefend-user-public/src/app/views/pages/auth/AuthPage.jsx
@@ -8,32 +8,35 @@ const AuthPage = () => {
   const location = useLocation();
 
   return (
-    <section className="access log-component">
-      <div className="container">
-        <div className="brand">
-          <Logo theme={"shadow"} />
-        </div>
-        <div className="forms">
-          <div className="nav">
-            <span
-              className={location.pathname === "/auth/signin" ? "active" : ""}
-            >
-              <Link to="/auth/signin">access</Link>
-            </span>
-            <span
-              className={
-                location.pathname.startsWith("/auth/signup") ? "active" : ""
-              }
-            >
-              <Link to="/auth/signup">new user</Link>
-            </span>
+    <>
+      <div className="codefend-img-bg"></div>
+      <section className="access log-component">
+        <div className="container">
+          <div className="brand">
+            <Logo theme={"shadow"} />
           </div>
-          <Suspense>
-            <Outlet />
-          </Suspense>
+          <div className="forms">
+            <div className="nav">
+              <span
+                className={location.pathname === "/auth/signin" ? "active" : ""}
+              >
+                <Link to="/auth/signin">access</Link>
+              </span>
+              <span
+                className={
+                  location.pathname.startsWith("/auth/signup") ? "active" : ""
+                }
+              >
+                <Link to="/auth/signup">new user</Link>
+              </span>
+            </div>
+            <Suspense>
+              <Outlet />
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 

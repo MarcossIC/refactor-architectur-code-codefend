b72c16368b7648a08f954093906c04f63392288a
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-01-03 16:31:41 -0300

    fix select and field size

--- codefend-user-public/src/app/RouterLayout.jsx
+++ codefend-user-public/src/app/RouterLayout.jsx
@@ -7,7 +7,7 @@ const Navbar = lazy(() => import("./views/components/standalones/Navbar"));
 const Sidebar = lazy(() => import("./views/components/standalones/Sidebar"));
 
 export const RouterLayout = () => {
-  const { isAuth } = useSelector((state) => state); //esto viene de redux store
+  const isAuth = useSelector((state) => state); //esto viene de redux store
 
   //este componente es el Outlet, envuelve a la applicacion
   return isAuth ? (

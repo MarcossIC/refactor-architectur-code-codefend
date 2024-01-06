3116727b23c4d7ab01c06bd95434fd19e53ce1c9
matti909 <matias.saantiago@gmail.com>
2024-01-06 13:36:07 -0300

    signup fixed

--- codefend-user-public/src/app/views/pages/auth/components/FinishsignUp.tsx
+++ codefend-user-public/src/app/views/pages/auth/components/FinishsignUp.tsx
@@ -1,88 +1,84 @@
+import { useState } from 'react'
+import { useParams } from 'react-router'
 
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-//Core packages
-import { createSignal, createEffect, Show, For, lazy } from "solid-js";
-import { AuthService } from "../../Services/ApiHandlerV2/auth.handler.js";
-import toast from "solid-toast";
-import { useParams } from "@solidjs/router";
-import ButtonLoader from "../ButtonLoader/buttonLoader.jsx";
-import jwt_decode from "jwt-decode";
-import history from "../../history.jsx";
-import { setAuth } from "../../utils/helper";
-import createUser from "../../Store/user";
-const Logo = lazy(() => import("../Logo/logo.jsx"));
+import { useAppSelector } from 'src/app/data'
 
 const FinishSignUpLayout = () => {
-  const { setUser } = createUser;
-  const [email, setEmail] = createSignal("");
-  const [password, setPassword] = createSignal("");
-  const [confirmPassword, setConfirmPassword] = createSignal("");
-  const [isLoading, setIsLoading] = createSignal(false);
-  const { ref } = useParams();
+  const [userState, setUserState] = useState({
+    user: createUser.user,
+    email: '',
+    password: '',
+    confirmPassword: '',
+    isLoading: false
+  })
+  const { ref } = useParams()
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
 
-    if (password() !== confirmPassword()) {
+    if (userState.password !== userState.confirmPassword) {
       return toast.error(
-        "Password does not match, Kindly check and try again !!!"
-      );
+        'Password does not match, Kindly check and try again !!!'
+      )
     }
-    if (!email() || email() < 0 || email() > 50) {
-      return toast.error("Invalid username");
+    if (!userState.email || userState.email.length < 0 || userState.email.length > 50) {
+      return toast.error('Invalid username')
     }
 
     if (!password() || password().length < 0 || password().length > 50) {
-      console.log({ pass: password() });
-      return toast.error("Invalid password");
+      console.log({ pass: password() })
+      return toast.error('Invalid password')
     }
 
     const requestParams = {
       username: email(),
       password: password(),
-      lead_reference_number: ref,
-    };
+      lead_reference_number: ref
+    }
 
-    setIsLoading(true);
+    setIsLoading(true)
 
     return AuthService.registerFinishHandler(requestParams)
       .then((response) => {
         if (response?.data?.error && response.data.error != 0) {
-          return toast.error(response.data.info);
+          return toast.error(response.data.info)
         }
 
         if (response.status != 200) {
-          return toast.error("An error has occurred...");
+          return toast.error('An error has occurred...')
         }
 
         if (!response.data.session) {
-          return toast.error("Invalid token response...");
+          return toast.error('Invalid token response...')
         }
 
         if (!response.data.user) {
-          return toast.error("Invalid user response...");
+          return toast.error('Invalid user response...')
         }
 
-        toast.success("Successfully Added User...");
+        toast.success('Successfully Added User...')
 
-        const decodedToken = jwt_decode(response.data.session);
-        const userData = { ...response.data.user, exp: decodedToken.exp ?? 0 };
-        setAuth(response.data.session, userData);
-        setUser(userData);
+        const decodedToken = jwt_decode(response.data.session)
+        const userData = { ...response.data.user, exp: decodedToken.exp ?? 0 }
+        setAuth(response.data.session, userData)
+        setUser(userData)
 
-        return history.push("/");
+        return history.push('/')
       })
       .finally(() => {
-        setIsLoading(false);
-      });
-  };
+        setIsLoading(false)
+      })
+  }
 
   return (
     <>
-      <section class="access log-component">
+      <section className="access log-component">
         <div className="container">
           <div className="brand">
-            <Logo theme={"shadow"} />
+            <Logo theme={'shadow'} />
           </div>
           <div className="forms">
             <div className="nav">
@@ -91,56 +87,56 @@ const FinishSignUpLayout = () => {
               </span>
             </div>
             <form onSubmit={handleSubmit}>
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   type="email"
                   onChange={(e) => {
-                    setEmail(e.target.value);
+                    setEmail(e.target.value)
                   }}
-                  class="full-w"
+                  className="full-w"
                   placeholder="Select Username"
                   required
                 />
               </div>
 
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   type="password"
                   onChange={(e) => {
-                    setPassword(e.target.value);
+                    setPassword(e.target.value)
                   }}
-                  class="full-w"
+                  className="full-w"
                   placeholder="Select Password"
                   required
                 />
               </div>
 
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   type="password"
                   onChange={(e) => {
-                    setConfirmPassword(e.target.value);
+                    setConfirmPassword(e.target.value)
                   }}
-                  class="full-w"
+                  className="full-w"
                   placeholder="Select Confirm Password"
                   required
                 />
               </div>
 
-              <div class="mt-6">
-                <span href="#" class="text-sm text-alt3">
-                  I have read and accept the <u>Privacy Policy</u> and{" "}
+              <div className="mt-6">
+                <span href="#" className="text-sm text-alt3">
+                  I have read and accept the <u>Privacy Policy</u> and{' '}
                   <u>Terms of Use.</u>
                 </span>
               </div>
-              <div class="mt-6">
+              <div className="mt-6">
                 <button
                   onClick={(e) => {
-                    handleSubmit(e);
+                    handleSubmit(e)
                   }}
                   disabled={isLoading()}
                   type="submit"
-                  class="btn btn-primary flex items-center gap-x-2"
+                  className="btn btn-primary flex items-center gap-x-2"
                 >
                   {isLoading() && <ButtonLoader />}
                   proceed
@@ -151,7 +147,7 @@ const FinishSignUpLayout = () => {
         </div>
       </section>
     </>
-  );
+  )
 }
 
-export default FinishSignUpLayout;
+export default FinishSignUpLayout

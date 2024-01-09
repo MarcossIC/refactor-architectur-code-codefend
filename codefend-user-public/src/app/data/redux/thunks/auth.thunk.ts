578e527a37ffcda760ed94892f4e557e82286a93
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-01-09 11:50:21 -0300

    merge with matias branch

--- codefend-user-public/src/app/data/redux/thunks/auth.thunk.ts
+++ codefend-user-public/src/app/data/redux/thunks/auth.thunk.ts
@@ -1,47 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { User, RegResponse, RegFinishResponse } from '../..';
-import { AuthServices } from '../../services';
-
-export interface LoginParams {
-	email: string;
-	password: string;
-}
-
-//paso 1
-export interface RegisterParams {
-	lead_fname: string;
-	lead_lname?: string;
-	lead_role: string | number;
-	lead_email: string;
-	lead_phone?: string;
-	company_name: string;
-	company_web: string;
-	company_size: string | number;
-	company_area: string;
-	phase: string;
-}
-
-// Tipo de retorno de la función de inicio de sesión
-export interface LoginResponse {
-	user?: User;
-	token?: string;
-	response: string;
-	message?: string;
-}
-
-// Tipo de retorno de la función de registro
-export interface RegisterResponse {
-	_id: string;
-	username: string;
-	email: string;
-	password: string;
-	role: string;
-	name: string;
-	companySize: string;
-	companyRole: string;
-	companyWeb: string;
-	companyCountry: string;
-}
+import { AuthServices } from '../../services/auth.service';
+import {
+	LoginParams,
+	LoginResponse,
+	RegFinishResponse,
+	RegResponse,
+	RegisterParams,
+} from '../..';
 
 export const loginThunk = createAsyncThunk<
 	LoginResponse,
--- codefend-user-public/src/app/views/pages/panel/layouts/web/WebApplication.tsx
+++ codefend-user-public/src/app/views/pages/panel/layouts/web/WebApplication.tsx
@@ -3,11 +3,11 @@ import { WebApplicationResources } from './components/WebApplicationResources';
 import { WebApplicationLocation } from './components/WebApplicationLocation';
 import { WebApplicationStatics } from './components/WebApplicationStatics';
 import { WebApplicationCredentials } from './components/WebApplicationCredentials';
+import { useWebapplication } from '../../../../../data';
 import '../../../../styles/card.scss';
 import '../../../../styles/table.scss';
 import '../../../../styles/flag.scss';
 import './webapplication.scss';
-import { useWebapplication } from '../../../../../data';
 
 const WebApplicationView: React.FC = () => {
 	const [showScreen, setShowScreen] = useState(false);
